23c1d243df3fe52a590e1f7840837c73511e82fc
Matt Cole <ccmatt19@gmail.com>
2023-03-23 14:34:24 -0400

    feat: generate programming tutorial example

---
+++ generate.py
@@ -0,0 +1,25 @@
+
+import os
+import openai
+import random
+
+random.seed(10)
+
+PROGRAMMING_LANGUAGES = ["Python", "Go", "Java", "TypeScript"]
+PROGRAMMING_CONCEPTS = ["Variables", "Functions", "Object Oriented Programming", "Polymorphism", "Control Structures"]
+
+openai.api_key = os.getenv("OPENAI_SECRET_KEY")
+
+def generate_tutorial(concept: str, language: str):
+    prompt = f"Explain {concept} with a single example in {language}"
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message.content
+
+tutorial = generate_tutorial(random.choice(PROGRAMMING_CONCEPTS), random.choice(PROGRAMMING_LANGUAGES))
+print(tutorial)
+
